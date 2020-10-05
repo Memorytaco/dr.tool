bf999a9e3ccb53ce49476fd63b3935b5bdf0492e
Memorytaco <Memorytoco@gmail.com>
2020-10-05 17:23:14 +0800

    Multiple file changed, Update cmd, elf
    cmd.c and cmd.h:
        add dependency relationship between options, and now user
        can accept extra arguments from cmd_match function using
        argvidx and argvctx macros.
    elf.c and elf.h:
        modify structure definition. May need further working.
    main.c:
        use the new functionality to process file value.
    dtool.h:
        add version info and author info.

--- main.c
+++ main.c
@@ -1,10 +1,10 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
 #include <stdint.h>
 #include <errno.h>
+#include <ctype.h>  // for isalpha some functions
 
 #include "cmd.h"
 #include "dtool.h"
@@ -13,71 +13,169 @@
 #include "util/file.h"
 
 struct store {
-  char* file;
   char* name;
   bool syntax;
+  bool ascii;
 };
 
-int cmd_call_help(struct cmdarg* arg, void* data, void* store) {
-  printf("Hello World, this is %s help option.\n", arg->opt & cmd_opt_long ? "long":"short");
-  return 0;
+void cmd_call_help(struct cmdargt* arg, void* data, void* store) {
+  char *color = setcmd('m', 3, 38, 5, 153);
+  char *endcmd = setcmd('m', 1, 0);
+  printf("Author: Memorytaco, github: github.com/Memorytaco.\n");
+  printf("Version " DTOOLVERSION ".\n");
+  printf("dtool <command> [option [arguments]]...\n");
+  printf("  command: elf, disas\n");
+  printf("  %self%s   [-h,--help,-ascii,-sec] file\n", color, endcmd);
+  printf("  %sdisas%s [-h,--help,-att,--name] file\n", color, endcmd);
+  free(color);
+  free(endcmd);
 }
 
-int cmd_call_file(struct cmdarg* arg, void* data, void* store) {
+void cmd_call_name(struct cmdargt* arg, void* data, void* store) {
   struct store* ptr = store;
-  ptr->file = arg->val;
-  printf("Specific file is %s\n", arg->val);
-  return 0;
+  size_t len = 0;
+  ptr->name = cmdargt_fetch_val(arg, &len);
 }
 
-int cmd_call_name(struct cmdarg* arg, void* data, void* store) {
-  struct store* ptr = store;
-  ptr->name = arg->val;
-  printf("Got a parser name %s\n", ptr->name);
-  return 0;
-}
-
-int cmd_call_syntax(struct cmdarg* arg, void* data, void* store) {
+void cmd_call_syntax(struct cmdargt* arg, void* data, void* store) {
   struct store* ptr = store;
   ptr->syntax = true;
-  return 0;
+}
+
+void cmd_call_setascii(struct cmdargt* arg, void* data, void* store) {
+  struct store* ptr = store;
+  ptr->ascii = true;
+}
+
+void show_ascii(unsigned char* buf, size_t offset, size_t sz)
+{
+  for (size_t i = 0; i<sz; i++) {
+    if (isgraph(buf[i+offset]))
+      printf("%c", buf[i+offset]);
+    else
+      printf(".");
+  }
+}
+
+void show_hex(unsigned char* buf, int num, int length) {
+  int anchor = 0;
+  printf("0x%08X ", 0);
+  for (int i = 0; i < length; i++) {
+    if (i%num == 0 && i != 0) {
+      printf("   ");
+      show_ascii(buf, anchor, num);
+      printf("\n");
+      printf("0x%08X ", i);
+      anchor = i;
+    }
+    if (i%4 == 3)
+      printf("%02X ", buf[i]);
+    else
+      printf("%02X", buf[i]);
+  }
+  if (length%num != 0) {
+    size_t len = length - anchor;
+    len = num*2 + (num/4) + 3 - len * 2 - len/4;
+    unsigned char *buf = malloc(len);
+    memset(buf, ' ', len);
+    printf("%s", buf);
+    show_ascii(buf, anchor, length - anchor);
+    printf("\n");
+    free(buf);
+  }
+}
+
+void cmd_call_elfsec
+( struct cmdargt* arg
+, void* data
+, void* store
+) {
+  struct store* ptr = store;
+  char *file = NULL;
+  if (argvctx(data) <= 0) return;
+  else file = argvidx(data, 0);
+  char* buffer = readfile(file, NULL);
+  struct sectbl table = build_sectbl(buffer);
+  free(buffer);
+  size_t len = 0;
+  int index = atoi(cmdargt_fetch_val(arg, &len));
+  if (table.num < index) {
+    printf( "number of entries %d,"
+        " index from %d ~ %d.\n"
+        , table.num, 0, table.num-1);
+    return;
+  }
+  union unisechdr hdr = {table.ents[index].hdr};
+  size_t size = 0;
+  if (table.class == 32) size = hdr.shdr_32->sh_size;
+  else size = hdr.shdr_64->sh_size;
+  if (size == 0) {
+    printf("The number %d Section has size 0.\n", index);
+    return;
+  }
+  if (ptr->ascii) {
+    show_hex(table.ents[index].sec, 16, size);
+  } else {
+    fwrite(table.ents[index].sec, 1, size, stdout);
+  }
 }
 
 int main(int argc, char** argv)
 {
-  char *color = setcmd('m', 3, 38, 5, 153);
-  char *endcmd = setcmd('m', 1, 0);
   if (argc < 2) {
-    printf("%s%s {command} [option]%s\n", argv[0], color, endcmd);
+    cmd_call_help(NULL, NULL, NULL);
     return 0;
   }
 
-  if (strcmp("disas", argv[1])) {
-    printf("disas is the only command.\n");
-    return 0;
-  }
-
-  struct cmd_reg regs[] = {
-    { .code = 0, .arg = { .opt = cmd_opt_short | cmd_opt_toggle, .key = "h" }, .call = cmd_call_help },
-    { .code = 0, .arg = { .opt = cmd_opt_long | cmd_opt_toggle, .key = "help" }, .call = cmd_call_help },
-    { .code = 2, .arg = { .opt = cmd_opt_long, .key = "file" }, .call = cmd_call_file },
-    { .code = 3, .arg = { .opt = cmd_opt_long, .key = "name" }, .call = cmd_call_name },
-    { .code = 4, .arg = { .opt = cmd_opt_short | cmd_opt_toggle, .key = "att" }, .call = cmd_call_syntax},
-    { .code = -1, .call = NULL}
+  struct cmdregt regs[] = {
+    { .key = "h", .code = 1, .opt = cmd_opt_short | cmd_opt_toggle
+    , .call = cmd_call_help
+    },
+    { .key = "help", .code = 2
+    , .opt = cmd_opt_long | cmd_opt_toggle, .call = cmd_call_help
+    },
+    { .key = "name", .code = 4, .opt = cmd_opt_long
+    , .call = cmd_call_name
+    },
+    { .key = "att", .code = 5
+    , .opt = cmd_opt_short | cmd_opt_toggle
+    , .call = cmd_call_syntax
+    },
+    { .key = "ascii", .code = 6
+    , .opt = cmd_opt_short | cmd_opt_toggle
+    , .call = cmd_call_setascii
+    },
+    { .key = "sec", .code = 7
+    , .opt = cmd_opt_short
+    , .call = cmd_call_elfsec
+    , .dep = {
+        .opt = {6, 0}
+      }
+    },
+    { .key = NULL, .code = 0, .call = NULL }
   };
   struct store* store = malloc(sizeof(struct store));
   memset(store, 0, sizeof(struct store));
-  cmd_match(argc-2, argv+2, regs, store);
+  arglst extra = cmd_match(argc-2, argv+2, regs, store);
 
-  if (store->file) {
-    char* name = store->name;
-    if (!name) {
-      name = store->file;
+  if (strcmp("disas", argv[1])) {
+    if (strcmp("elf", argv[1])) {
+      printf("Avaliable commands: disas, elf.\n");
     }
-    size_t size=0;
-    char* buf = readfile(store->file, &size);
-    dt_disas(name, buf, size, store->syntax);
-  } else printf("Please specific file option\n");
+    return 0;
+  }
+
+  char *file = NULL;
+  if (argvctx(extra) <= 0) {
+    printf("Please specific one file.\n");
+  } else file = argvidx(extra, 0);
+  char* name = store->name;
+  if (!name) {
+    name = file;
+  }
+  size_t size=0;
+  char* buf = readfile(file, &size);
+  dt_disas(name, buf, size, store->syntax);
 
   return 0;
 }
