f9bdd7630f4e44799b669ab2dde9bad0a3d73f8f
Memorytaco <Memorytoco@gmail.com>
2020-10-28 22:21:03 +0800

    Apply Multiple Bug Fixs to vara.c and vara.h
    Fix heap pollution in checkbits, add VType size when computing
    memory size for bits and keep a sane space like 8B for unchecking
    memory.
    
    Fix vptr macro. remove parenthese between '*'.
    
    Refactor Type bits access macros. Now it's all symbol names no
    nesty hex number.
    
    Refactor those is* macros, so all is* macros can have one single
    logic domain and will not intercept with others.
    
    Add Warning for using vptr, and give two examples. user may take
    it as simple guidlines.
    
    Add vaddr for vara_ptr as shorthand. It's recommended to use
    macros instead of the vara_ptr function call.

--- src/vara.h
+++ src/vara.h
@@ -4,7 +4,6 @@
 #include <stdarg.h>
 #include <string.h>
 
-struct vara;
 typedef struct vara* vtra;
 
 // oO octet/signed octet 8bit
@@ -16,19 +15,33 @@ typedef struct vara* vtra;
 
 // a handy macro for using generic parameter as
 // normal variable.
+// e.g.
+//  #1: vptr(unsigned int, st, 1, 0) = 3;
+//  #2: *vptr(unsigned int*, st, 1, 0) = 3;
 //
-// warning: don't use it if you don't know the details
-// of the structure. otherwise it will may generate
-// memory segment fault.
+//  #1 is different with #2. #1 interpret the memory as
+//  unsigned int, but #2 take it as a pointer which points to
+//  unsigned int. this is quite different, so be careful.
+//
+// warning: only use this macro when you are aware of action
+//          you are taking.
+// use it with !!!PACKED!!! data structure, or it will surprise
+// you. Anyway be careful with using data structure along with
+// this macro.
 #define vptr(type, store, depth, ...) \
-  (*((type)*)vara_ptr(store, depth, NULL, __VA_ARGS__))
+  (*(type *)vara_ptr(store, depth, NULL, __VA_ARGS__))
+
+// short hand for vara_ptr.
+#define vaddr(store, depth, ...) \
+  vara_ptr(store, depth, NULL, __VA_ARGS__)
 
 // "name:@dw(w[16]2)@"
-vtra  vara_alloc(const char* dsc);
+// allocate the memory access holder.
+vtra  vara_alloc(const char* dsc);  // TODO: add align support
 void* vara_ptr(vtra store, int depth, size_t* size, ...);
 const char* vara_name(vtra store);
-void  vara_free(vtra store);
-void  vara_info(vtra store);
+void  vara_free(vtra store);  // TODO: add vara_free
+void  vara_info(vtra store);  // TODO: finish vara_info
 
 
 #endif
