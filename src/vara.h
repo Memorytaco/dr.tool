4700bc474da2dfb617ac3cdc960a15867bf78343
Memorytaco <Memorytoco@gmail.com>
2020-10-30 21:39:44 +0800

    Release version to 0.2.0
    Add more specific info for vara interface in vara.h.
    
    Update version to 0.2.0 and Change tool interface.
    
    Signed-off-by: Dunor <dunor@dunor.dev>

--- src/vara.h
+++ src/vara.h
@@ -6,13 +6,6 @@
 
 typedef struct vara* vtra;
 
-// oO octet/signed octet 8bit
-// wW : word
-// dD : dword
-// qQ : qword
-// @  : pointer
-// !@#$%^&*()[]{}?/.>,<;:"'
-
 // a handy macro for using generic parameter as
 // normal variable.
 // e.g.
@@ -35,12 +28,72 @@ typedef struct vara* vtra;
 #define vaddr(store, depth, ...) \
   vara_ptr(store, depth, NULL, __VA_ARGS__)
 
-// "name:@dw(w[16]2)@"
+/*
+ * Syntax:
+ *  Type := Name ":" Qualifier+
+ *  Name := [A~Za~z0-9_]+
+ *  Qualifier := Primitive Suffix? Qualifier*
+ *            | "(" Qualifier+ ")" Qualifier*
+ *  Primitive := [OoWwDdQq]
+ *  Suffix := ArrySuffix Digit? | Digit
+ *  ArrySuffix := "[" Digit "]" ArrySuffix*
+ *  Digit := [0-9]
+ *
+ * e.g.
+ *  struct Type1 {
+ *    int i;
+ *    unsigned j;
+ *  };  == "Type1:DD"
+ *
+ * struct Type2 {
+ *    struct Type1 pre;
+ *    struct Type1 *next;
+ *  }; == "Type2:(DD)@"
+ *
+ * struct Type3 {
+ *    int i[20];
+ *    int j[20];
+ *    int k[20];
+ *  }; == "Type3:D[20]3"
+ *     or "Type3:D[20]D[20]D[20]"
+ *
+ */
+
+/*
+ *
+ * oO : octet/signed octet 8bit
+ * wW : word
+ * dD : dword
+ * qQ : qword
+ * @  : pointer
+ *
+ * lower case should be used to denote signed version.
+ * and uppger case should be used to denote unsigned verison.
+ *
+ * But They are no difference currently, because they are
+ * used only to denote the storage size. And you need to
+ * specific the way how to inteprete these memory bits via
+ * type cast in c using a void pointer or just the vptr
+ * pointer.
+ *
+ * vara_ptr will return the storage size via @size parameter
+ * if you want to directly use the call.
+ *
+ * use vara_free if you don't need it anymore.
+ *
+ * vara_info will output info of the type for debuging
+ * purpose only.
+ *
+ * vara_name return the type, but this call may be deprecated
+ * in future.
+ *
+ */
+
 // allocate the memory access holder.
 vtra  vara_alloc(const char* dsc);  // TODO: add align support
 void* vara_ptr(vtra store, int depth, size_t* size, ...);
 const char* vara_name(vtra store);
-void  vara_free(vtra store);  // TODO: add vara_free
+void  vara_free(vtra store);
 void  vara_info(vtra store);  // TODO: finish vara_info
 
 
